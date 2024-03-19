fc4244838fdba2dcafe21a47fc6bccf2ee9c7ca7
Victor Chagas <42676345+VictorChagasS@users.noreply.github.com>
2024-03-19 14:37:07 -0300

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import socket
 
 NAMESERVICE_IP = "192.0.2.100"
 NAMESERVICE_PORT = 53
-WEBSERVICE_IP = "203.0.113.200"
+WEBSERVICE_IP = "192.168.1.101"
 WEBSERVICE_PORT = 80
 
 def print_service_info():
@@ -15,7 +15,7 @@ def print_service_info():
 
 def start_traffic_capture():
     print("Iniciando captura de trafego...")
-    os.system("sudo tcpdump -i any -w traffic_capture_" + os.getlogin() + ".pcap &")
+    os.system("sudo tcpdump -i any -w traffic_capture_VictorChagas.pcap &")
 
 def test_host_connectivity():
     response = os.system("ping -c 1 " + NAMESERVICE_IP)
@@ -26,20 +26,26 @@ def test_host_connectivity():
 
 def test_service_response():
     print("Testando servico de Nome...")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
         result = s.connect_ex((NAMESERVICE_IP, NAMESERVICE_PORT))
         if result == 0:
             print("Servico de Nome esta respondendo corretamente.")
         else:
             print("Nao foi possivel conectar ao servico de Nome.")
+    finally:
+        s.close()
 
     print("Testando servico Web...")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
         result = s.connect_ex((WEBSERVICE_IP, WEBSERVICE_PORT))
         if result == 0:
             print("Servico Web esta respondendo corretamente.")
         else:
             print("Nao foi possivel conectar ao servico Web.")
+    finally:
+        s.close()
 
 def stop_traffic_capture():
     print("Encerrando captura de trafego...")
