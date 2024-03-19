b39c18530d69e2b5905020ff2f1d40942a5cc879
Victor Chagas <42676345+VictorChagasS@users.noreply.github.com>
2024-03-19 14:51:29 -0300

    Update run.py

--- run.py
+++ run.py
@@ -15,30 +15,30 @@ def imprimir_informacoes_servico():
 
 def iniciar_captura_trafego():
     nome_arquivo = "captura_trafego_VictorChagas.pcap"
-    print("Iniciando captura de tráfego...")
+    print("Iniciando captura de trafego...")
     os.system("sudo tcpdump -i any -w " + nome_arquivo + " &")
 
 def testar_conectividade_host(endereco_ip):
     resposta = os.system("ping -c 1 " + endereco_ip)
     if resposta == 0:
-        print("O host", endereco_ip, "está online.")
+        print("O host", endereco_ip, "esta online.")
     else:
-        print("O host", endereco_ip, "está offline.")
+        print("O host", endereco_ip, "esta offline.")
 
 def testar_resposta_servico(endereco_ip, porta, nome_servico):
-    print("Testando serviço", nome_servico, "...")
+    print("Testando servico", nome_servico, "...")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         resultado = s.connect_ex((endereco_ip, porta))
         if resultado == 0:
-            print("O serviço", nome_servico, "está respondendo corretamente.")
+            print("O servico", nome_servico, "esta respondendo corretamente.")
         else:
-            print("Não foi possível conectar ao serviço", nome_servico)
+            print("Nao foi possivel conectar ao servico", nome_servico)
     finally:
         s.close()
 
 def encerrar_captura_trafego():
-    print("Encerrando captura de tráfego...")
+    print("Encerrando captura de trafego...")
     os.system("sudo pkill tcpdump")
 
 def main():
